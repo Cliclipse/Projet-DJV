cdc5bec84bb52ddf256e61ba151e5f86d0803147
Cliclipse <120763901+Cliclipse@users.noreply.github.com>
2025-11-24 15:04:56 +0100

    debut controller

--- ProjetDJV1/DJV Shooter Project/Assets/Scripts/PlayerController.cs
+++ ProjetDJV1/DJV Shooter Project/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speedAcceleration = 0.5f;
+    [SerializeField] private float initialSpeed = 10f;
+    [SerializeField] private float maxSpeed = 30f;
+    [SerializeField] private float jumpForce = 10f;
+    
+    
+    [SerializeField] private int maxHealth = 3;
+    
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] private KeyCode leftKey = KeyCode.A;
+    [SerializeField] private KeyCode rightKey = KeyCode.D;
+    [SerializeField] private KeyCode upKey = KeyCode.W;
+    [SerializeField] private KeyCode downKey = KeyCode.S;
+    
+    private Rigidbody _rb;
+    
+    private int _currentHealth;
+
+
+    private void MovementManager()
     {
-        
+        if (Input.GetKey(leftKey)) _rb.velocity -= 0.5f * transform.right * speedAcceleration;
+        if (Input.GetKey(rightKey)) _rb.velocity += 0.5f * transform.right * speedAcceleration;
+        if (Input.GetKey(downKey)) _rb.velocity -= 0.5f * transform.up * speedAcceleration;
+        if (Input.GetKey(upKey)) _rb.velocity += 0.5f * transform.up * speedAcceleration;
+    }
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _currentHealth = maxHealth;
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
